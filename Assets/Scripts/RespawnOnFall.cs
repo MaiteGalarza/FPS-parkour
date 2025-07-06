42cc119d726e8b3e1b5e56c8f8b6816fc12b8eee
MaiteGalarza <49189070@est.ort.edu.ar>
2025-07-05 21:08:11 -0300

    d

--- Assets/Scripts/RespawnOnFall.cs
+++ Assets/Scripts/RespawnOnFall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RespawOnFall : MonoBehaviour
+public class RespawnOnFall : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
